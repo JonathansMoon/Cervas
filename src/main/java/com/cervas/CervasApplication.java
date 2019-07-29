0c133c444b00bae3821c9880e653b80ed417254d
Osaopmds <osaopmds@pm.df.gov.br>
2019-07-29 18:43:52 -0300

    first commit

---
+++ src/main/java/com/cervas/CervasApplication.java
@@ -0,0 +1,13 @@
+package com.cervas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CervasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CervasApplication.class, args);
+	}
+
+}
