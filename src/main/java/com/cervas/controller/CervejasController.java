d87a4c243ce8ad7b6286a5a62535da87c69a8d17
Osaopmds <osaopmds@pm.df.gov.br>
2019-07-31 11:19:56 -0300

    Adição de novo atributo e validação

--- src/main/java/com/cervas/controller/CervejasController.java
+++ src/main/java/com/cervas/controller/CervejasController.java
@@ -1,6 +1,10 @@
 package com.cervas.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,12 +14,18 @@ import com.cervas.model.Cerveja;
 public class CervejasController {
 	
 	@RequestMapping("/cervejas/novo")
-	public String novo() {
+	public String novo(Model model) {
+		model.addAttribute(new Cerveja());
 		return "cerveja/CadastroCerveja";
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(Cerveja cerveja) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+		
+		// Result é um objeto que guarda infomações de erro
+		if (result.hasErrors()) {
+			return "cerveja/CadastroCerveja";
+		}
 		System.out.println(">>>> sku:" +cerveja.getSku());
 		return "cerveja/CadastroCerveja";
 	}
--- src/main/java/com/cervas/model/Cerveja.java
+++ src/main/java/com/cervas/model/Cerveja.java
@@ -1,10 +1,26 @@
 package com.cervas.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Cerveja {
 
+	@NotBlank(message = "SKU é Obrigatório")
 	private String sku;
+	
+	@NotBlank(message = "Nome é Obrigatório")
 	private String nome;
 	
+	@NotBlank(message = "Descrição é Obrigatório")
+	@Size(max = 50)
+	private String descricao;
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 	public String getSku() {
 		return sku;
 	}
