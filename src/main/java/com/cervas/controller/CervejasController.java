15ce3f430e30675f1fea80466a267791fe774402
Jonathan Gomes <jonathanx56@hotmail.com>
2019-08-15 18:30:06 -0300

    formatação do formulário

--- src/main/java/com/cervas/controller/CervejasController.java
+++ src/main/java/com/cervas/controller/CervejasController.java
@@ -39,7 +39,7 @@ public class CervejasController {
 
 	@RequestMapping("/cervejas/cadastro")
 	public String cadastro() {
-		return "cerveja/cadastro-produto";
+		return "layout/LayoutPadrao";
 	}
 
 }
