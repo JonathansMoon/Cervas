cfb0f166a4763aaa1d208d38b401401d2b4e119a
osaopmds <osaopmds@PM03600218762.pmdf.local>
2019-10-15 18:53:24 -0300

    Refatoramento do input.photos.js para melhor organizacao

--- src/main/resources/static/custom/javascripts/input.photos.js
+++ src/main/resources/static/custom/javascripts/input.photos.js
@@ -1,90 +1,84 @@
-var bar = document.getElementById('js-progressbar');
+	var Cervas = Cervas || {};
+	var bar = document.getElementById('js-progressbar');
 
-    $(function() {
-    	var settings = {
-    			
-			url: 'fotos',
-	        type:'json',
-	        allow: '*.(jpg|jpeg|png)',
-	        multiple: false,
-	
-	        beforeSend: function () {
-	            console.log('beforeSend', arguments);
-	        },
-	        beforeAll: function () {
-	            console.log('beforeAll', arguments);
-	        },
-	        load: function () {
-	            console.log('load', arguments);
-	        },
-	        error: function () {
-	            console.log('error', arguments);
-	        },
-	        complete: function () {
-	            console.log('complete', arguments);
-	        },
-	
-	        loadStart: function (e) {
-	            console.log('loadStart', arguments);
-	
-	            bar.removeAttribute('hidden');
-	            bar.max = e.total;
-	            bar.value = e.loaded;
-	        },
-	
-	        progress: function (e) {
-	            console.log('progress', arguments);
-	
-	            bar.max = e.total;
-	            bar.value = e.loaded;
-	        },
-	
-	        loadEnd: function (e) {
-	            console.log('loadEnd', arguments);
-	
-	            bar.max = e.total;
-	            bar.value = e.loaded;
-	        },
-	
-	        completeAll: function (resposta) {
+	Cervas.UploadFoto = ( function () {
+		//Declaro Variaveis
+		function UploadFoto() {
 
-				var inputNomeFoto = $('input[name=foto]');
-				var inputContentType =  $('input[name=contentType]');
+			this.inputNomeFoto = $('input[name=foto]');
+			this.inputContentType =  $('input[name=contentType]');
 
-				// Usando a biblioteca HandleBars.js para 
-				// gerar variaveis js e utiliza-las no template
-				var deOndeVemoHtml = $('#foto-cerveja').html();
-				var template = Handlebars.compile(deOndeVemoHtml);
-				//Gera o objeto dentro de um template
-				var htmlFotoCerveja = template({nomeFoto: resposta.response.nome});
+			// Usando a biblioteca HandleBars.js para 
+			// gerar variaveis js e utiliza-las no template
+			this.deOndeVemoHtml = $('#foto-cerveja').html();
+			this.template = Handlebars.compile(this.deOndeVemoHtml);
+			this.containerFotoCerveja = $('.js-container-foto-cerveja');
 
-				var containerFotoCerveja = $('.js-container-foto-cerveja');
+			this.inputUploud = $('#js-upload');
+		}
 
-				var inputUploud = $('#js-upload');
+		//Inicio o método
+		UploadFoto.prototype.iniciar = function () {
+			var settings = {
+				url: 'fotos',
+				type:'json',
+				allow: '*.(jpg|jpeg|png)',
+				multiple: false,
 
-				inputUploud.addClass('hidden');
+				loadStart: function (e) {
+					bar.removeAttribute('hidden');
+					bar.max = e.total;
+					bar.value = e.loaded;
+			   },
+	   
+			   progress: function (e) {
+				   bar.max = e.total;
+				   bar.value = e.loaded;
+			   },
+	   
+			   loadEnd: function (e) {
+					bar.max = e.total;
+					bar.value = e.loaded;
+			   },
+				// O this permite usar variaveis acima
+				complete: onUploadComplete.bind(this)
+			}
+			UIkit.upload($('.js-upload'), settings);
+		}
 
-				inputNomeFoto.val(resposta.response.nome);
-				inputContentType.val(resposta.response.contentType);
-				
-				//Inclui o template HandleBars dentro da class selecionada 
-				containerFotoCerveja.append(htmlFotoCerveja);
+		//Metodo chamado ao completar o envio
+		function onUploadComplete(resposta) {
+			this.inputUploud.addClass('hidden');
 
-				$('.js-remove-foto').on('click', function() {
-					$('.js-foto-cerveja').remove();
-					inputUploud.removeClass('hidden');
-					inputNomeFoto.val();
-					inputContentType.val();
-				});
+			this.inputNomeFoto.val(resposta.response.nome);
+			this.inputContentType.val(resposta.response.contentType);
+			
+			//Gera o objeto dentro de um template
+			var htmlFotoCerveja = this.template({nomeFoto: resposta.response.nome});
+			//Inclui o template HandleBars dentro da class selecionada 
+			this.containerFotoCerveja.append(htmlFotoCerveja);
 
-				setTimeout(function () {
-	                bar.setAttribute('hidden', 'hidden');
-				}, 1000);		
-	        }
-    			
-    	};
-    	
-        UIkit.upload($('.js-upload'), settings);
+			$('.js-remove-foto').on('click', onRemoveFoto.bind(this));
 
-    });
-    
+			setTimeout(function () {
+				bar.setAttribute('hidden', 'hidden');
+			}, 1000);
+		}
+
+		//Método chamado ao excluir
+		function onRemoveFoto () {
+			$('.js-foto-cerveja').remove();
+			this.inputUploud.removeClass('hidden');
+			this.inputNomeFoto.val();
+			this.inputContentType.val();
+		}
+
+		return UploadFoto;
+
+	})();
+
+
+	$(function () {
+		var uploadFoto = new Cervas.UploadFoto();
+		uploadFoto.iniciar();
+	});
