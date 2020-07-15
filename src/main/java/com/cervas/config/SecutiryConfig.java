9846e7d8ff487416032ba3606de2a28166e0556c
jonathan.gomes <jonathanx56@hotmail.com>
2020-07-15 12:00:41 -0300

    config spring auth

--- src/main/java/com/cervas/config/SecutiryConfig.java
+++ src/main/java/com/cervas/config/SecutiryConfig.java
@@ -3,6 +3,7 @@ package com.cervas.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,9 +15,29 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
+		.withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
 	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/layout/**");
+			
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
 
+		http.authorizeRequests()
+		.antMatchers("/layout/**").permitAll()
+			.anyRequest().authenticated()
+				.and()
+			.formLogin().loginPage("/login")
+			.permitAll()
+				.and()
+			.csrf().disable();
+	}
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 	    return new BCryptPasswordEncoder();
---
+++ src/main/java/com/cervas/controller/SegurancaController.java
@@ -0,0 +1,13 @@
+package com.cervas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SegurancaController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "Login";
+	}
+}
